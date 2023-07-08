a4e906e549eccc15a72d6068ddfb2d827f34d75d
stephen911 <stephendappah1@gmail.com>
2023-07-08 18:51:46 +0000

    cvv

--- functions.php
+++ functions.php
@@ -140,7 +140,7 @@ function resetpass($email)
             ';
             // yolk mailer
             // $mym = [$email];
-            $from = ['Tucee', 'info@gnaccportal.com'];
+            $from = ['GNACC', 'GNACC@gnaccportal.com'];
             $headers = 'MIME-Version: 1.0'."\r\n";
             $headers .= 'Content-type: text/html; charset=iso-8859-1'."\r\n";
             $headers .= 'From: '.$from[1];
