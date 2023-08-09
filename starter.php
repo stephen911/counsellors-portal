c77f8b881719741036d453421ffc4d9ce9e08452
Stephen Dapaah <58371485+stephen911@users.noreply.github.com>
2023-08-09 10:00:24 +0000

    Update starter.php

--- starter.php
+++ starter.php
@@ -2,7 +2,7 @@
 
 
 <?php
- $conn = mysqli_connect('localhost', 'tucevmlk_counsellor', 'tgb)IL$q,0I}', 'tucevmlk_counsellor') or die('failed');
+ $conn = mysqli_connect('localhost', 'scribbed_gnacc', 'Teamwork@2019', 'scribbed_gnacc') or die('failed');
 // $conn = mysqli_connect('127.0.0.1', 'root', '', 'counsellor') or die('failed');
 ?>
 
