7711cab582cf9931816f2c99a5e480d94c95876a
Stephen Dapaah <58371485+stephen911@users.noreply.github.com>
2023-08-10 14:00:47 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <meta charset="utf-8" />
     <title>Log In | GNACC </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
-    <meta content="Coderthemes" name="author" />
+    <meta content="Gnacc portal" name="description" />
+    <meta content="Stephen Dapaah" name="author" />
 
     <!-- App favicon -->
     <link rel="shortcut icon" href="assets/images/favicon2.ico">
